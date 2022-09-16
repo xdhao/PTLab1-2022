5ee1ced4114570cf58287fba6ecb807977609ed1
Ivanov Ivan Ivanovich <Ivanov@example.com>
2022-09-16 14:51:01 +0300

    add simple commentary

--- src/main.py
+++ src/main.py
@@ -19,6 +19,8 @@ def main():
 
     reader = TextDataReader()
     students = reader.read(path)
+
+    # вывод студентов
     print("Students: ", students)
 
     rating = CalcRating(students).calc()
