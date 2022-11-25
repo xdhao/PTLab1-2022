d1e146677944a35353a14ca29d20752ec6572bfe
Stas Zhabunin <63603336+xdhao@users.noreply.github.com>
2022-11-25 16:19:35 +0300

    Update test_QuartileCalc.py

--- test/test_QuartileCalc.py
+++ test/test_QuartileCalc.py
@@ -36,8 +36,7 @@ class TestQuartileRating:
 
     @pytest.fixture()
     def input_data_two(self) -> tuple[DataType, list]:
-        data: DataType = {"Иванов Иван Иванович": [("math", 80),
-                            ("prin", 90), ("litra", 76)],
+        data: DataType = {"Иванов Иван Иванович": [("math", 80), ("prin", 90), ("litra", 76)],
                           "Петров Петр Петрович":
                               [("math", 100), ("soc", 90), ("chemistry", 61)],
                           "Петров Иван Петрович":
