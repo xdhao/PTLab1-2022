18ea0ff03ada6f602c95a389acc55acd5e71935a
Stas Zhabunin <63603336+xdhao@users.noreply.github.com>
2022-11-25 15:05:54 +0300

    style fix

--- src/FirstQuartileCalc.py
+++ src/FirstQuartileCalc.py
@@ -6,6 +6,7 @@ class FirstQuartileCalc(CalcRating):
 
     def quartile_calc(self) -> list:
         data = self.calc()
-        sorted_rating = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
+        sorted_rating = dict(sorted(data.items(),
+                                    key=lambda item: item[1], reverse=True))
         quan = len(sorted_rating) // 4
         return list(sorted_rating.items())[:quan]
--- test/test_QuartileCalc.py
+++ test/test_QuartileCalc.py
@@ -49,10 +49,12 @@ class TestQuartileRating:
             assert pytest.approx(rating_score,
                                  abs=0.001) == input_data[1][student]
 
-    def test_calc_quartile(self, input_data: tuple[DataType, RatingsType]) -> None:
+    def test_calc_quartile(self,
+                           input_data: tuple[DataType, RatingsType]) -> None:
 
         rating = FirstQuartileCalc(input_data[0]).calc()
         res = FirstQuartileCalc(input_data[0]).quartile_calc()
-        sorted_rating = dict(sorted(rating.items(), key=lambda item: item[1], reverse=True))
+        sorted_rating = dict(sorted(rating.items(),
+                                    key=lambda item: item[1], reverse=True))
         quan = len(sorted_rating) // 4
         assert res == list(sorted_rating.items())[:quan]
